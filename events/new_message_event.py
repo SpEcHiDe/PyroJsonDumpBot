6bcf6e622d5cac7d72487d9ee169939c62b5f00b
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-12-17 22:37:30 +0530

    add the message wrapped in <code>

---
+++ events/new_message_event.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+LOGGER = logging.getLogger(__name__)
+
+
+import os
+import time
+
+
+async def new_message_event(client, message):
+    try:
+        await message.reply_text(
+            f"<code>{message}</code>",
+            quote=True,
+            disable_web_page_preview=True,
+            disable_notification=True,
+        )
+    except Exception as e:
+        work_area = os.path.join(
+            Config.TMP_DOWNLOAD_DIRECTORY,
+            str(time.time())
+        )
+        # create download directory, if not exist
+        if not os.path.isdir(work_area):
+            os.makedirs(work_area)
+        temp_writing_file = os.path.join(
+            work_area,
+            "json.text"
+        )
+        with open(temp_writing_file, "w+", encoding="utf8") as out_file:
+            out_file.write(str(message))
+        await message.reply_document(
+            document=temp_writing_file,
+            caption=str(e),
+            disable_notification=True,
+            quote=True
+        )
+        os.remove(temp_writing_file)
--- robot.py
+++ robot.py
@@ -12,7 +12,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 import os
-import time
+
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
@@ -24,35 +24,7 @@ import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-async def new_message_event(client, message):
-    try:
-        await message.reply_text(
-            message,
-            quote=True,
-            disable_web_page_preview=True,
-            disable_notification=True,
-        )
-    except Exception as e:
-        work_area = os.path.join(
-            Config.TMP_DOWNLOAD_DIRECTORY,
-            str(time.time())
-        )
-        # create download directory, if not exist
-        if not os.path.isdir(work_area):
-            os.makedirs(work_area)
-        temp_writing_file = os.path.join(
-            work_area,
-            "json.text"
-        )
-        with open(temp_writing_file, "w+", encoding="utf8") as out_file:
-            out_file.write(str(message))
-        await message.reply_document(
-            document=temp_writing_file,
-            caption=str(e),
-            disable_notification=True,
-            quote=True
-        )
-        os.remove(temp_writing_file)
+from events.new_message_event import new_message_event
 
 
 if __name__ == "__main__":
